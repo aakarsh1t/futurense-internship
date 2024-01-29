80e46889f9b8de980b10b54bbf3af4d8c02bcdd4
Aakarshit Rathore <asr3648@gmail.com>
2024-01-29 10:39:31 +0530

    28 - 01 -2024

--- Action Items/Project/Tic tac toe/tic tac toe.py
+++ Action Items/Project/Tic tac toe/tic tac toe.py
@@ -220,22 +220,24 @@ def play():
 	wpl = partial(withplayer, menu)
 
 	head = Button(menu, text="---Welcome to tic-tac-toe---",
-				activeforeground='red',
-				activebackground="yellow", bg="red",
-				fg="yellow", width=500, font='summer', bd=5)
+				activeforeground='white',
+				activebackground="red", bg="white",
+				fg="black", width=500, font='summer', bd=5)
 
 	B1 = Button(menu, text="Single Player", command=wpc,
-				activeforeground='red',
-				activebackground="yellow", bg="red",
-				fg="yellow", width=500, font='summer', bd=5)
+				activeforeground='white',
+				activebackground="red", bg="white",
+				fg="black", width=500, font='summer', bd=5)
 
-	B2 = Button(menu, text="Multi Player", command=wpl, activeforeground='red',
-				activebackground="yellow", bg="red", fg="yellow",
-				width=500, font='summer', bd=5)
+	B2 = Button(menu, text="Multi Player", command=wpl,
+				activeforeground='white',
+				activebackground="red", bg="white",
+				fg="black", width=500, font='summer', bd=5)
 
-	B3 = Button(menu, text="Exit", command=menu.quit, activeforeground='red',
-				activebackground="yellow", bg="red", fg="yellow",
-				width=500, font='summer', bd=5)
+	B3 = Button(menu, text="Exit", command=menu.quit,
+				activeforeground='white',
+				activebackground="red", bg="white",
+				fg="black", width=500, font='summer', bd=5)
 	head.pack(side='top')
 	B1.pack(side='top')
 	B2.pack(side='top')
